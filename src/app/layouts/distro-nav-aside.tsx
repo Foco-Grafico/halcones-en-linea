4dfbfbf8185f50f73752dbfc2f606c3ed61a85dd
KikeDeAlba <likingvirus1@gmail.com>
2024-06-19 13:09:29 -0700

    Scroll in Aside Fix

--- src/app/layouts/distro-nav-aside.tsx
+++ src/app/layouts/distro-nav-aside.tsx
@@ -1,22 +1,22 @@
-import { SideBarV2 } from '@/components/sidebar/sidebar'
-import { SideBarOptions } from '@/components/sidebar/types'
+import { SideBarV2 } from "@/components/sidebar/sidebar";
+import type { SideBarOptions } from "@/components/sidebar/types";
 
 interface Props {
-  children: React.ReactNode
-  navbar: React.JSX.Element
-  options: SideBarOptions
+	children: React.ReactNode;
+	navbar: React.JSX.Element;
+	options: SideBarOptions;
 }
 
 export const DistroNavASide = ({ children, navbar, options }: Props) => {
-  return (
-    <div className='h-screen flex flex-col '>
-      {navbar}
-      <div className='flex flex-1 overflow-hidden'>
-        <SideBarV2 options={options} />
-        <div className='flex-1 h-full bg-gradient-to-tr from-[#1f5186] to-[#131a2d] overflow-y-auto px-11 py-8'>
-          {children}
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className="h-screen flex flex-col ">
+			{navbar}
+			<div className="flex flex-1 overflow-hidden">
+				<SideBarV2 options={options} />
+				<div className="flex-1 h-full bg-gradient-to-tr from-[#1f5186] to-[#131a2d] overflow-y-auto px-11 py-8">
+					{children}
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,32 +1,25 @@
-import { SideBarMultiItem } from './sidebar-item'
-import { v4 } from '@/utils/uuid'
-import { SideBarOptions } from './types'
+import { SideBarMultiItem } from "./sidebar-item";
+import { v4 } from "@/utils/uuid";
+import type { SideBarOptions } from "./types";
+import { ShyScrollbar } from "../utils";
 
 interface Props {
-  options: SideBarOptions
+	options: SideBarOptions;
 }
 
 export const SideBarV2 = ({ options }: Props) => {
-  return (
-    <aside
-      className='bg-[#cdcccb] h-full w-80'
-    >
-
-      <ul className='flex flex-col gap-1'>
-
-        {
-          options.map((o) => (
-            <SideBarMultiItem
-              subItems={o.sub}
-              key={v4()}
-            >
-              {o.title}
-            </SideBarMultiItem>
-          ))
-        }
-
-      </ul>
-
-    </aside>
-  )
-}
+	return (
+		<aside
+			style={ShyScrollbar}
+			className="bg-[#cdcccb] h-full w-80 overflow-y-auto"
+		>
+			<ul className="flex flex-col gap-1">
+				{options.map((o) => (
+					<SideBarMultiItem subItems={o.sub} key={v4()}>
+						{o.title}
+					</SideBarMultiItem>
+				))}
+			</ul>
+		</aside>
+	);
+};
