819298f78479d37fb2891aab23ceaac6cb8e7500
KikeDeAlba <likingvirus1@gmail.com>
2024-06-19 17:35:03 -0700

    register user change feedback message

--- src/app/admin/components/register-form.tsx
+++ src/app/admin/components/register-form.tsx
@@ -1,134 +1,149 @@
-'use client'
+"use client";
 
-import { Form, LabeledInput, SubmitButton } from '@/components/utils'
-import { updateAccountInfo } from '@/services/supabase/actions/admin/auth'
-import { register } from '@/services/supabase/client'
-import { USER_TYPES } from '@/services/supabase/functions/types'
-import { Account } from '@/services/supabase/types'
-import { useRouter } from 'next/navigation'
-import { toast } from 'sonner'
+import { Form, LabeledInput, SubmitButton } from "@/components/utils";
+import { updateAccountInfo } from "@/services/supabase/actions/admin/auth";
+import { register } from "@/services/supabase/client";
+import type { USER_TYPES } from "@/services/supabase/functions/types";
+import type { Account } from "@/services/supabase/types";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface Props {
-  role: USER_TYPES
-  redirect?: string
-  defaultValues?: Account
-  from: 'students' | 'professor' | 'coordinators'
+	role: USER_TYPES;
+	redirect?: string;
+	defaultValues?: Account;
+	from: "students" | "professor" | "coordinators";
 }
 
-export const RegisterForm = ({ role, redirect, defaultValues, from }: Props) => {
-  const { push } = useRouter()
+export const RegisterForm = ({
+	role,
+	redirect,
+	defaultValues,
+	from,
+}: Props) => {
+	const { push } = useRouter();
+	const [message, setMessage] = useState<{
+		message: string;
+		type: "success" | "error";
+	} | null>(null);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setMessage(null);
 
-    const execute = defaultValues != null
-      ? () => {
-          const data = new FormData(e.currentTarget)
+		const execute =
+			defaultValues != null
+				? () => {
+						const data = new FormData(e.currentTarget);
 
-          updateAccountInfo(data, from)
-            .catch((err) => {
-              console.error(err)
+						updateAccountInfo(data, from).catch((err) => {
+							console.error(err);
 
-              toast.error('Error al actualizar usuario')
-            })
-        }
-      : () => {
-          register({
-            birthdate: new Date(e.currentTarget.birthdate.value),
-            email: e.currentTarget.email.value,
-            firstName: e.currentTarget.first_name.value,
-            lastName: e.currentTarget.last_name.value,
-            password: e.currentTarget.password.value,
-            phone: e.currentTarget.phone.value,
-            role
-          })
-            .then(() => {
-              toast.success('Usuario registrado')
+							setMessage({
+								message: "Error al actualizar usuario",
+								type: "error",
+							});
+						});
+					}
+				: () => {
+						register({
+							birthdate: new Date(e.currentTarget.birthdate.value),
+							email: e.currentTarget.email.value,
+							firstName: e.currentTarget.first_name.value,
+							lastName: e.currentTarget.last_name.value,
+							password: e.currentTarget.password.value,
+							phone: e.currentTarget.phone.value,
+							role,
+						})
+							.then(() => {
+								setMessage({
+									message: "Usuario registrado",
+									type: "success",
+								});
 
-              if (redirect != null) {
-                push(redirect)
-              }
-            })
-            .catch((err) => {
-              console.error(err)
+								if (redirect != null) {
+									push(redirect);
+								}
+							})
+							.catch((err) => {
+								console.error(err);
 
-              toast.error('Error al registrar usuario')
-            })
-        }
+								setMessage({
+									message: "Error al registrar usuario",
+									type: "error",
+								});
+							});
+					};
 
-    execute()
+		execute();
 
-    e.currentTarget.reset()
-  }
+		e.currentTarget.reset();
+	};
 
-  return (
-    <Form className='flex flex-col h-full gap-2 p-3 .' onSubmit={handleSubmit}>
-      {
-        defaultValues != null && (
-          <input
-            type='hidden'
-            name='owner'
-            value={defaultValues.owner ?? ''}
-          />
-        )
-      }
+	return (
+		<Form className="flex flex-col h-full gap-2 p-3 ." onSubmit={handleSubmit}>
+			{defaultValues != null && (
+				<input type="hidden" name="owner" value={defaultValues.owner ?? ""} />
+			)}
 
-      <LabeledInput
-        label='Nombres'
-        name='first_name'
-        placeholder='Jose'
-        defaultValue={defaultValues?.first_name ?? ''}
-      />
+			<LabeledInput
+				label="Nombres"
+				name="first_name"
+				placeholder="Jose"
+				defaultValue={defaultValues?.first_name ?? ""}
+			/>
 
-      <LabeledInput
-        label='Apellidos'
-        name='last_name'
-        placeholder='Perez Leon'
-        defaultValue={defaultValues?.last_name ?? ''}
-      />
+			<LabeledInput
+				label="Apellidos"
+				name="last_name"
+				placeholder="Perez Leon"
+				defaultValue={defaultValues?.last_name ?? ""}
+			/>
 
-      <LabeledInput
-        label='Correo'
-        name='email'
-        type='email'
-        placeholder='example@example.com'
-        defaultValue={defaultValues?.email ?? ''}
-      />
+			<LabeledInput
+				label="Correo"
+				name="email"
+				type="email"
+				placeholder="example@example.com"
+				defaultValue={defaultValues?.email ?? ""}
+			/>
 
-      {
-        defaultValues == null && (
-          <LabeledInput
-            label='Contraseña'
-            name='password'
-            type='password'
-            placeholder='********'
-          />
-        )
-      }
+			{defaultValues == null && (
+				<LabeledInput
+					label="Contraseña"
+					name="password"
+					type="password"
+					placeholder="********"
+				/>
+			)}
 
-      <LabeledInput
-        label='Telefono'
-        name='phone'
-        type='tel'
-        placeholder='1234567890'
-        defaultValue={defaultValues?.phone ?? ''}
-      />
+			<LabeledInput
+				label="Telefono"
+				name="phone"
+				type="tel"
+				placeholder="1234567890"
+				defaultValue={defaultValues?.phone ?? ""}
+			/>
 
-      {
-        defaultValues == null && (
-          <LabeledInput
-            label='Fecha de nacimiento'
-            name='birthdate'
-            type='date'
-          />
-        )
-      }
+			{defaultValues == null && (
+				<LabeledInput
+					label="Fecha de nacimiento"
+					name="birthdate"
+					type="date"
+				/>
+			)}
 
-      <SubmitButton className='bg-white text-[#1a63a5] hover:bg-[#cdcccb] focus:ring-white rounded py-2'>
-        {
-          defaultValues == null ? 'Registrar' : 'Actualizar'
-        }
-      </SubmitButton>
-    </Form>
-  )
-}
+			<SubmitButton className="bg-white text-[#1a63a5] hover:bg-[#cdcccb] focus:ring-white rounded py-2">
+				{defaultValues == null ? "Registrar" : "Actualizar"}
+			</SubmitButton>
+
+			{message != null && (
+				<div
+					className={`text-white ${message.type === "success" ? "bg-green-500" : "bg-red-500"} px-2 py-1 rounded-md bg-opacity-50 text-center mt-4 animate-fade-in animate-duration-150`}
+				>
+					{message.message}
+				</div>
+			)}
+		</Form>
+	);
+};
--- src/app/admin/professor/new/page.tsx
+++ src/app/admin/professor/new/page.tsx
@@ -1,35 +1,36 @@
-import { USER_TYPES } from '@/services/supabase/functions/types'
-import { RegisterForm } from '@/app/admin/components/register-form'
-import { Main, H1, FormSection } from '@/components/utils'
-import { getAccount } from '@/services/supabase/actions/auth'
+import { USER_TYPES } from "@/services/supabase/functions/types";
+import { RegisterForm } from "@/app/admin/components/register-form";
+import { Main, H1, FormSection } from "@/components/utils";
+import { getAccount } from "@/services/supabase/actions/auth";
 
 interface Props {
-  params?: {
-    id: string
-  }
+	params?: {
+		id: string;
+	};
 }
 
-export default async function NewProfessorPage ({ params }: Props) {
-  const editMode = params?.id != null
+export default async function NewProfessorPage({ params }: Props) {
+	const editMode = params?.id != null;
 
-  const professor = editMode ? await getAccount(params?.id ?? '') : null
+	const professor = editMode ? await getAccount(params?.id ?? "") : null;
 
-  return (
-    <Main>
-
-      <FormSection>
-        <div className='flex flex-col items-center justify-centers'>
-          <H1 className='text-white '>
-            {editMode ? 'Editar profesor' : 'Crear nuevo profesor'}
-          </H1>
-          <a className='text-white'>Ingresa los datos del nuevo profesor</a>
-        </div>
-        <RegisterForm
-          defaultValues={professor ?? undefined}
-          from='professor' role={USER_TYPES.PROFESSOR}
-        />
-      </FormSection>
-
-    </Main>
-  )
+	return (
+		<Main>
+			<FormSection>
+				<div className="flex flex-col items-center justify-centers">
+					<H1 className="text-white ">
+						{editMode ? "Editar profesor" : "Crear nuevo profesor"}
+					</H1>
+					<span className="text-white">
+						Ingresa los datos del nuevo profesor
+					</span>
+				</div>
+				<RegisterForm
+					defaultValues={professor ?? undefined}
+					from="professor"
+					role={USER_TYPES.PROFESSOR}
+				/>
+			</FormSection>
+		</Main>
+	);
 }
