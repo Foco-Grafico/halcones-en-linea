87359249a2b9a7c250ffebe8024516a31d2f6186
KikeDeAlba <likingvirus1@gmail.com>
2024-06-22 12:11:23 -0700

    change color

--- src/app/student/[career]/subject/[slug]/activities/components/activity-link.tsx
+++ src/app/student/[career]/subject/[slug]/activities/components/activity-link.tsx
@@ -1,23 +1,23 @@
-'use client'
+"use client";
 
-import { IconPlayerPlay } from '@tabler/icons-react'
-import Link from 'next/link'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { IconPlayerPlay } from "@tabler/icons-react";
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
 
 interface Props {
-  activityId: number
+	activityId: number;
 }
 
 export const ActivityLink = ({ activityId }: Props) => {
-  const pathname = usePathname()
-  const seachParams = useSearchParams()
+	const pathname = usePathname();
+	const seachParams = useSearchParams();
 
-  return (
-    <Link
-      href={`${pathname}/do/${activityId}?${seachParams.toString()}`}
-      className='bg-[#808080] px-3 py-1 flex items-center justify-center rounded-r-lg'
-    >
-      <IconPlayerPlay size={24} />
-    </Link>
-  )
-}
+	return (
+		<Link
+			href={`${pathname}/do/${activityId}?${seachParams.toString()}`}
+			className="bg-[#bdc141] px-3 py-1 flex items-center justify-center rounded-r-lg"
+		>
+			<IconPlayerPlay size={24} />
+		</Link>
+	);
+};
