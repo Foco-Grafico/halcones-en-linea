9890d918ae0139ce3567a934dbd0a2b0ea9c9dfd
KikeDeAlba <likingvirus1@gmail.com>
2024-06-22 11:33:30 -0700

    fix background box color in create activity

--- src/app/professor/create-activity/components/selector-type.tsx
+++ src/app/professor/create-activity/components/selector-type.tsx
@@ -1,52 +1,55 @@
-'use client'
+"use client";
 
-import { Enums } from 'database.types'
+import type { Enums } from "database.types";
 
 interface Props {
-  onSelect?: (type: Enums<'activity_type'>) => void
+	onSelect?: (type: Enums<"activity_type">) => void;
 }
 
 export const SelectorType = ({ onSelect }: Props) => {
-  return (
-    <div className='flex flex-col w-full gap-2 h-full justify-center items-center'>
-      <Button
-        onClick={() => {
-          onSelect?.('trivia')
-        }}
-      >
-        Trivia
-      </Button>
-      <Button
-        onClick={() => {
-          onSelect?.('exam')
-        }}
-      >
-        Examen
-      </Button>
-      <Button
-        onClick={() => {
-          onSelect?.('questionary')
-        }}
-      >
-        Cuestionario
-      </Button>
-      <Button
-        onClick={() => {
-          onSelect?.('work')
-        }}
-      >
-        Trabajo
-      </Button>
-    </div>
-  )
-}
+	return (
+		<div className="flex flex-col w-full gap-2 h-full justify-center items-center">
+			<Button
+				onClick={() => {
+					onSelect?.("trivia");
+				}}
+			>
+				Trivia
+			</Button>
+			<Button
+				onClick={() => {
+					onSelect?.("exam");
+				}}
+			>
+				Examen
+			</Button>
+			<Button
+				onClick={() => {
+					onSelect?.("questionary");
+				}}
+			>
+				Cuestionario
+			</Button>
+			<Button
+				onClick={() => {
+					onSelect?.("work");
+				}}
+			>
+				Trabajo
+			</Button>
+		</div>
+	);
+};
 
-const Button = ({ children, ...props }: React.HTMLAttributes<HTMLButtonElement>) => (
-  <button
-    type='button'
-    className='bg-itesus-tertiary w-full rounded-md text-left px-5 font-bold text-lg'
-    {...props}
-  >
-    {children}
-  </button>
-)
+const Button = ({
+	children,
+	...props
+}: React.HTMLAttributes<HTMLButtonElement>) => (
+	<button
+		type="button"
+		className="bg-[#aaaaa9] w-full rounded-md text-left px-5 font-bold text-lg"
+		{...props}
+	>
+		{children}
+	</button>
+);
